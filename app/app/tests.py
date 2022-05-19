f67dcd6b86acb6e91c9d69b4fd898d06b21647c6
okrimad <okrimad@tiscali.it>
2022-05-19 05:06:52 +0200

    Initial commit on orphan branch

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from app.calc import add, substract
+
+
+class CalcTest(TestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test taht values are subtracted and returned"""
+        self.assertEqual(substract(5, 11), 6)
